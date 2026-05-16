cbe80384a1574f9f80c38c261b288f5c07658ff1
wertyg775 <danishshaifuddin.DL@gmail.com>
2026-05-16 23:01:25 +0800

    feat: headers for token

---
+++ include/token.h
@@ -0,0 +1,27 @@
+#ifndef COOL_TOKEN_H
+#define COOL_TOKEN_H
+
+typedef enum {
+    TOKEN_EOF,
+
+    TOKEN_CLASS,
+    TOKEN_TYPE_IDENTIFIER,
+
+    TOKEN_LBRACE,
+    TOKEN_RBRACE,
+    TOKEN_SEMICOLON,
+
+    TOKEN_ERROR  
+} TokenType;
+
+typedef struct {
+    TokenType type;
+    const char *start;
+    int length;
+    int line;
+    int column;
+} Token;
+
+const char *token_type_name(TokenType type);
+
+#endif
