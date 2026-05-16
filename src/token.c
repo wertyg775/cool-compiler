a0986d234b050ebdde3d6d8c35ae47e14e198d9c
wertyg775 <danishshaifuddin.DL@gmail.com>
2026-05-16 23:01:55 +0800

    feat: added token type namings

---
+++ src/token.c
@@ -0,0 +1,22 @@
+#include "token.h"
+
+const char *token_type_name(TokenType type) {
+    switch (type) {
+        case TOKEN_EOF:
+            return "TOKEN_EOF";
+        case TOKEN_CLASS:
+            return "TOKEN_CLASS";
+        case TOKEN_TYPE_IDENTIFIER:
+            return "TOKEN_TYPE_IDENTIFIER";
+        case TOKEN_LBRACE:
+            return "TOKEN_LBRACE";
+        case TOKEN_RBRACE:
+            return "TOKEN_RBRACE";
+        case TOKEN_SEMICOLON:
+            return "TOKEN_SEMICOLON";
+        case TOKEN_ERROR:
+            return "TOKEN_ERROR";
+        default:
+            return "TOKEN_UNKNOWN";
+    }
+}
