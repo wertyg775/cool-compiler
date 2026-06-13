89e8923a98abb0d41a7f3a4b07dbc7095e3a6c3f
wertyg775 <danishshaifuddin.DL@gmail.com>
2026-06-13 15:52:46 +0800

    feat: added new token types

--- src/token.c
+++ src/token.c
@@ -14,6 +14,14 @@ const char *token_type_name(TokenType type) {
             return "TOKEN_LBRACE";
         case TOKEN_RBRACE:
             return "TOKEN_RBRACE";
+        case TOKEN_LBRACKET:
+            return "TOKEN_LBRACKET";
+        case TOKEN_RBRACKET:
+            return "TOKEN_RBRACKET";    
+        case TOKEN_COMMA:
+            return "TOKEN_COMMA";
+        case TOKEN_DOT:
+            return "TOKEN_DOT";
         case TOKEN_SEMICOLON:
             return "TOKEN_SEMICOLON";
         case TOKEN_ERROR:
